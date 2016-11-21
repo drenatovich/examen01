5be2bedf1c137ae5cceb0c6edd2792881ef2b158
Damir Galimov <drenatovich@gmail.com>
2016-11-21 15:40:29 +0100

    finalizado

--- src/Putadon.java
+++ src/Putadon.java
@@ -12,30 +12,32 @@ import java.util.Scanner;
  */
 public class Putadon {
 	
+	static Scanner sc=new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		//Variables inicialización
 		String frase="";
-		int numPos=0;
-		int numRepeticiones=0;
+		int numPos=0, numRepeticiones=0;
 		char letra=0;
 		
 		//Pedimos datos
-		Scanner sc=new Scanner(System.in);
+		frase=pideDatos("Introduce tu frase");
 		
-		System.out.println("Introduce tu frase");
-		frase = sc.nextLine();
-		
-		System.out.println("Introduce la letra a buscar");
-		letra = sc.nextLine().charAt(0); //charAt(0) devuelve la primera letra
+		//charAt(0) devuelve la primera letra
+		letra = pideDatos("Introduce la letra a buscar").charAt(0);
 		
 		//Buscamos las letras
 		for(int i=0;i<frase.length();i++){
-			if(letra==frase.charAt(i-1)){
-				numRepeticiones=numRepeticiones;
-				System.out.println("Letra "+letra+" encontrada en la posicion "+i);
+			if(letra==frase.charAt(i)){
+				numRepeticiones=numRepeticiones+1;
+				System.out.println("Letra "+letra+" encontrada en la posicion "+(i+1));
 			}
 		}
 		System.out.println("Total repeticiones "+numRepeticiones);
-	
+	}
+	public static String pideDatos(String texto){
+		System.out.println(texto);
+		texto = sc.nextLine();
+		return texto;
 	}
 }
